48563b1d92e9770c6c5bfbe2211c4b85fa96354a
Saurabh Singh Bisht <bisht.singh750@gmail.com>
2023-04-25 18:03:38 +0530

    Initial Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setVal(100);
+        System.out.println(obj.getVal());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int val;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
 }
